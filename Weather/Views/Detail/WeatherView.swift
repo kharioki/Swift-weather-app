969fd26e7db08417aca68578e33da19cdef73539
kharioki <tony.kharioki@gmail.com>
2023-02-17 20:14:33 +0300

    weather widget

---
+++ Weather/Views/Detail/WeatherView.swift
@@ -0,0 +1,43 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Tony Kharioki on 17/02/2023.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        ZStack {
+            // MARK: Background
+            Color.background
+                .ignoresSafeArea()
+            
+            // MARK: Weather Widgets
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    ForEach(Forecast.cities) { forecast in
+                        WeatherWidget(forecast: forecast)
+                    }
+                }
+            }
+            .safeAreaInset(edge: .top) {
+                EmptyView()
+                    .frame(height: 110)
+            }
+        }
+        .overlay {
+            // MARK: Navigation Bar
+            NavigationBar()
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+            .preferredColorScheme(.dark)
+    }
+}
