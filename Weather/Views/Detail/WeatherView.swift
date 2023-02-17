a38f70ed629a25e57c047f9b97b95b3b5a325dcf
kharioki <tony.kharioki@gmail.com>
2023-02-17 21:23:04 +0300

    search bar

--- Weather/Views/Detail/WeatherView.swift
+++ Weather/Views/Detail/WeatherView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct WeatherView: View {
+    @State private var searchText = ""
+    
+    var searchResults: [Forecast] {
+        if searchText.isEmpty {
+            return Forecast.cities
+        } else {
+            return Forecast.cities.filter { $0.location.contains(searchText) }
+        }
+    }
+    
     var body: some View {
         ZStack {
             // MARK: Background
@@ -17,7 +27,7 @@ struct WeatherView: View {
             // MARK: Weather Widgets
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 20) {
-                    ForEach(Forecast.cities) { forecast in
+                    ForEach(searchResults) { forecast in
                         WeatherWidget(forecast: forecast)
                     }
                 }
@@ -29,15 +39,19 @@ struct WeatherView: View {
         }
         .overlay {
             // MARK: Navigation Bar
-            NavigationBar()
+            NavigationBar(searchText: $searchText)
         }
         .navigationBarHidden(true)
+//        .searchable(text: $searchText, placement:
+//            .navigationBarDrawer(displayMode: .always), prompt: "Search for a city...")
     }
 }
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherView()
-            .preferredColorScheme(.dark)
+        NavigationView {
+            WeatherView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
